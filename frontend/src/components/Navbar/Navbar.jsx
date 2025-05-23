4382df547a36626b5e6fc3bc77eaa1ee69bebb97
Ogbonna Blessed <blessedlyrics11@gmail.com>
2025-05-23 11:36:28 +0100

    feat: add home page banner

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from 'react'
 import { GiChefToque, GiForkKnifeSpoon } from "react-icons/gi";
 import { NavLink, useLocation, useNavigate } from 'react-router-dom';
