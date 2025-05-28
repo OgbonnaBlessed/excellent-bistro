8bd88c0e523342a0a326f555b71a1daec16b0605
Ogbonna Blessed <blessedlyrics11@gmail.com>
2025-05-28 14:39:46 +0100

    feat: add about page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Home from './pages/Home'
 import { Route, Routes } from 'react-router-dom'
 import Signup from './components/Signup/Signup'
 import Menu from './pages/Menu'
+import AboutPage from './pages/AboutPage'
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Route path='/login' element={<Home />} />
       <Route path='/signup' element={<Signup />} />
       <Route path='/menu' element={<Menu />} />
+      <Route path='/about' element={<AboutPage />} />
     </Routes>
   )
 }
