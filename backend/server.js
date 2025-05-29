5c695ca70ea259f9056ca26a383ed167f264a877
Ogbonna Blessed <blessedlyrics11@gmail.com>
2025-05-29 16:05:43 +0100

    feat: add user route

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config'
 import { connectDB } from './config/db.js';
+import userRoutes from './routes/user.route.js'
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -15,6 +16,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // ROUTES
+app.use('/api/user', userRoutes);
+
 app.get('/', (req, res) => {
     res.send('API WORKING');
 })
