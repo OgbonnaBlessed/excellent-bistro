c860cb88dc160074e39dc313d1c039a881f15000
Ogbonna Blessed <blessedlyrics11@gmail.com>
2025-05-29 17:26:46 +0100

    feat: add item model

---
+++ backend/models/item.model.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+const itemSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    category: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    rating: {
+        type: Number,
+        default: 0
+    },
+    hearts: {
+        type: Number,
+        default: 0
+    },
+    total: {
+        type: Number,
+        default: 0
+    },
+    imageUrl: { 
+        type: String 
+    }
+}, { timestamps: true });
+
+const itemModel = mongoose.model('item', itemSchema);
+
+export default itemModel;
