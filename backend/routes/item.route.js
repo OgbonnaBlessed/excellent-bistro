2520f7b94e0e3f1a96a09e74824b5f5922861a7d
Ogbonna Blessed <blessedlyrics11@gmail.com>
2025-05-29 18:19:50 +0100

    feat: add item route

---
+++ backend/routes/item.route.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import multer from 'multer';
+import { createItem, getItems, deleteItem } from '../controllers/item.controller.js';
+
+const router = express.Router();
+
+// IMAGE STORAGE
+const storage = multer.diskStorage({
+    destination: (_req, _file, cb) => cb(null, 'uploads/'),
+    filename: (_req, file, cb) => cb(null, `${Date.now()}-${file.fileoriginalname}`),
+})
+
+const upload = multer({ storage });
+
+router.post('/', upload.single('image'), createItem);
+router.get('/', getItems);
+router.delete('/:id', deleteItem);
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,15 @@ import cors from 'cors';
 import 'dotenv/config'
 import { connectDB } from './config/db.js';
 import userRoutes from './routes/user.route.js'
+import itemRoutes from './routes/item.route.js'
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express();
 const port = process.env.PORT || 4000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // DATABASE
 connectDB();
@@ -27,6 +33,8 @@ app.use(express.urlencoded({ extended: true }));
 
 // ROUTES
 app.use('/api/user', userRoutes);
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use('/api/items', itemRoutes);
 
 app.get('/', (req, res) => {
     res.send('API WORKING');
