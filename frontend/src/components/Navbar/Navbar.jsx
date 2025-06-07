322fdb8882fc992b722b3f51f72bf93f12d9bddb
Ogbonna Blessed <blessedlyrics11@gmail.com>
2025-06-07 13:50:44 +0100

    refactor: modify the navbar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -243,8 +243,8 @@ const Navbar = () => {
                                         className='w-full px-4 py-3 bg-gradient-to-b from-amber-500 to-amber-700 rounded-xl font-semibold flex items-center justify-center space-x-2 text-sm text-amber-100 hover:text-amber-300'
                                         onClick={() => setIsOpen(false)}
                                     >
-                                        <span className='text-lg'>{link.icon}</span>
-                                        <span className='text-base font-medium'>{link.name}</span>
+                                        <span>{link.icon}</span>
+                                        <span>{link.name}</span>
                                     </NavLink>
                                 ))}
                             </nav>
